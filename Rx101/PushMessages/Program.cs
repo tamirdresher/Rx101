8f705c5437cff321a18f95d699b766c632a31cbd
tamir.dresher@gmail.com <vhpupuyo>
2015-11-01 08:53:58 +0200

    refactoring

--- Rx101/PushMessages/Program.cs
+++ Rx101/PushMessages/Program.cs
@@ -10,7 +10,58 @@ namespace PushMessages
     {
         static void Main(string[] args)
         {
-            
+
+
+        }
+    }
+
+    class SocialNetworksManager
+    {
+        ISocialNetworkClient _facebook = new FakeFacebookClient();
+        ISocialNetworkClient _twitter = new FakeTwitterClient();
+        ISocialNetworkClient _linkedin = new FakeLinkedinClient();
+        IEnumerable<Message> LoadMessages(string hashtag)
+        {
+            var statuses = _facebook.Search(hashtag);
+            var tweets = _twitter.Search(hashtag);
+            var linkedinMsgs = _linkedin.Search(hashtag);
+            return statuses.Concat(tweets).Concat(linkedinMsgs);
+        }
+
+    }
+
+    class Message
+    {
+        public string Content { get; set; }
+    }
+
+    interface ISocialNetworkClient
+    {
+        IEnumerable<Message> Search(string hashtag);
+    }
+
+    class FakeFacebookClient : ISocialNetworkClient
+    {
+        public IEnumerable<Message> Search(string hashtag)
+        {
+            return Enumerable.Range(1, 10).Select(i => "Status" + i).Select(m=>new Message() {Content = m});
+        }
+    }
+
+    class FakeTwitterClient : ISocialNetworkClient
+    {
+        public IEnumerable<Message> Search(string hashtag)
+        {
+            return Enumerable.Range(1, 10).Select(i => "Tweet" + i).Select(m => new Message() { Content = m });
+
+        }
+    }
+
+    class FakeLinkedinClient : ISocialNetworkClient
+    {
+        public IEnumerable<Message> Search(string hashtag)
+        {
+            return Enumerable.Range(1, 10).Select(i => "Update" + i).Select(m => new Message() { Content = m });
 
         }
     }
---
+++ Rx101/ReactiveSearch/SearchServiceClient.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ReactiveSearch
+{
+    class SearchServiceClient
+    {
+        const string BaseAddress = "http://localhost.fiddler:2458/api/Search?searchTerm=";
+        private HttpClient _httpClient;
+
+        public SearchServiceClient()
+        {
+            
+
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        public async Task<IEnumerable<string>> SearchAsync(string searchTerm)
+        {
+            var response = await _httpClient.GetAsync(BaseAddress + searchTerm);
+            return await response.Content.ReadAsAsync<IEnumerable<string>>();
+        }
+    }
+}
